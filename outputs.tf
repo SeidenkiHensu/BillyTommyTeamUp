6c8ce917a7778090ec8c594c739ac81fbb0dad9a
xStatick <46169646+xStatick@users.noreply.github.com>
2025-07-03 16:02:05 -0700

    testing flip and renames

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-# Setting up an output so we can see the load balancer DNS name
+# Sets up an output so we can see the load balancer DNS name
 output "load_balancer_dns" {
   description = "The DNS name of the application load balancer."
-#  value       = aws_lb.app_lb.dns_name
-#  value       = aws_lb.app_lb[0].dns_name
   value       = length(aws_lb.app_lb) > 0 ? aws_lb.app_lb[0].dns_name : ""
 }
 
+# Sets up an output for the CloudWatch dashboard name
 output "cloudwatch_dashboard_name" {
   description = "The name of the CloudWatch dashboard for EC2 monitoring."
   value       = aws_cloudwatch_dashboard.ec2_dashboard.dashboard_name
 }
 
+# Sets up an output for the CloudWatch log group name
 output "cloudwatch_log_group_name" {
   description = "The name of the CloudWatch log group for EC2 logs."
   value       = aws_cloudwatch_log_group.ec2_log_group.name
