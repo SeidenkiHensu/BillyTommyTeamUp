2f94116e13c0a0c9384ff187f5a85d1eed90c0fd
xStatick <46169646+xStatick@users.noreply.github.com>
2025-07-03 11:16:58 -0700

    adding cloudwatch

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,13 @@ output "green_instance_public_ip" {
   description = "Public IP of the green environment EC2 instance."
   value       = [for instance in aws_instance.green : instance.public_ip]
 }
+
+output "cloudwatch_dashboard_name" {
+  description = "The name of the CloudWatch dashboard for EC2 monitoring."
+  value       = aws_cloudwatch_dashboard.ec2_dashboard.dashboard_name
+}
+
+output "cloudwatch_log_group_name" {
+  description = "The name of the CloudWatch log group for EC2 logs."
+  value       = aws_cloudwatch_log_group.ec2_log_group.name
+}
