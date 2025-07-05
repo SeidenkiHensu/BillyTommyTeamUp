ade819333317f2154b9d8ed2887c195bb9816dbc
xStatick <46169646+xStatick@users.noreply.github.com>
2025-07-04 21:03:59 -0700

    YOURE THIRTY SIX.5

--- iam-user.tf
+++ iam-user.tf
@@ -16,7 +16,7 @@ resource "aws_iam_user" "project_users" {
 
 # Setting up IAM User Policy Attachments. This will attach the AmazonEC2FullAccess policy to the user
 resource "aws_iam_user_policy_attachment" "ec2_full_access" {
-  for_each = aws_iam_user.project_users
+  for_each   = aws_iam_user.project_users
   user       = each.value.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
@@ -24,13 +24,13 @@ resource "aws_iam_user_policy_attachment" "ec2_full_access" {
 # Setting up IAM Access Keys
 resource "aws_iam_access_key" "user_keys" {
   for_each = aws_iam_user.project_users
-  user = each.value.name
+  user     = each.value.name
 }
 
 # Setting up IAM Access Key Outputs. This is just for demo purposes. In a real world scenario, we would be using secrets manager to store the access keys.
 output "iam_user_access_keys" {
   description = "Access keys for provisioned IAM users."
-  value = {
+  value       = {
     for name, key in aws_iam_access_key.user_keys :
     name => {
       access_key_id     = key.id
